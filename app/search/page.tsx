c3e1b96f63754b44f848c9e14aea47251cea40fa
Quang Minh <minhqnd@gmail.com>
2024-12-27 10:19:22 +0700

    breadcrumbs

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -6,13 +6,16 @@ import { Footer } from '@/components/footer'
 import { SearchHeader } from '@/components/search-header'
 import { FlightResults } from '@/components/bus-results'
 import { ProgressSteps } from '@/components/progress-steps'
+import { Breadcrumbs } from '@/components/breadcrumbs'
 
 export default function SearchPage() {
-    const [currentStep] = useState(1)
+    const [currentStep] = useState(3)
+    const [currentPage, setCurrentPage] = useState('FPT - Quảng Ninh')
 
     return (
         <main className="min-h-screen bg-[#FFF9F0]">
             <Navbar />
+            <Breadcrumbs currentPage={currentPage} />
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 min-h-screen">
                 <div className="mb-16 mx-auto md:w-4/5">
                     <ProgressSteps currentStep={currentStep} />
