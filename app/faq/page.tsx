5978ebda9e03d516a1cf1f6c1157c9b9a1ca2b54
Quang Minh <minhqnd@gmail.com>
2025-01-10 06:41:29 +0700

    fix faq page

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -6,7 +6,7 @@ export default function FaqPage() {
   return (
     <main className="min-h-screen bg-[#FFF9F0] flex flex-col items-center w-full bg-[url('/section-background.png')] bg-repeat">
       <Navbar />
-      <div className="container py-12">
+      <div className="container py-24">
         <div className="mx-auto px-4 max-w-3xl">
           <h1 className="mb-8 text-center text-3xl font-bold text-red-800">Câu hỏi thường gặp</h1>
           <FaqAccordion />
